926947acf08d6cf7ab82e7a3b740e5ab19ed1161
Franzé Jr <franzejr@gmail.com>
2018-01-24 15:58:22 -0300

    Use two parameters for urls

--- lib/exchequer_client/URL.rb
+++ lib/exchequer_client/URL.rb
@@ -5,7 +5,7 @@ module ExchequerClient
         "#{ExchequerClient::Config.base_url}/"
       end
 
-      def offers_show(offer_id, manager_public_token: nil)
+      def offers_show(offer_id, manager_public_token)
         "#{ExchequerClient::Config.base_url}/offers/#{offer_id}?public_token=#{manager_public_token}"
       end
     end
