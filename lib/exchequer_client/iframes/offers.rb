5b02d8f4e6e920ff2775bf8c595fbae520f51e1b
Franzé Jr <franzejr@gmail.com>
2018-01-25 18:18:33 -0300

    Do not use borders

--- lib/exchequer_client/iframes/offers.rb
+++ lib/exchequer_client/iframes/offers.rb
@@ -2,7 +2,7 @@ module ExchequerClient
   module Iframe
     class Offers
       def self.show(offer_id, manager_public_token)
-        "<iframe src='#{ExchequerClient::URL.offers_show(offer_id, manager_public_token)}' width='100%' height='100%'></iframe>"
+        "<iframe src='#{ExchequerClient::URL.offers_show(offer_id, manager_public_token)}' width='100%' height='100%' frameBorder='0'></iframe>"
       end
     end
   end
